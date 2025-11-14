33832498f48e88f080b11e8af0926b0e252e3968
Jamie Taylor <jamietaylor112@gmail.com>
2025-11-13 23:05:57 -0800

    Testing pre-commit

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,5 +2,6 @@ import { ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
+  //test test
   return twMerge(clsx(inputs));
 }
